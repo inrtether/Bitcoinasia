6cb97480671fc7885bf5505a15219638a4761205
salmonashish <36722739+salmonashish@users.noreply.github.com>
2018-03-02 18:42:17 +0530

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -91,7 +91,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "BitcoinAsia";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "010a01ff0001b595e3a3a817029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101a1c31635d5e96a0678a6fe3842b91b7d61ff91c8baea376abd877aee54c6b624";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -123,12 +123,7 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const char* const SEED_NODES[] = { 
-  "18.217.98.213:19356",
-  "18.217.109.16:19356",
-  "18.221.8.236:19356",
-  "18.220.201.153:19356",
-  "18.222.64.156:19356",
-  "18.221.14.2:19356",
+  "",
 };
 
 struct CheckpointData {
