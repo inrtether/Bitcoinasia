e0ba4f6de80f9e6c1a880071c1eb255678d212e0
salmonashish <36722739+salmonashish@users.noreply.github.com>
2018-03-02 16:34:24 +0530

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -35,6 +35,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 // MONEY_SUPPLY - total number coins to be generated
 const uint64_t MONEY_SUPPLY                                  = UINT64_C(210000000000000000);
 const uint64_t TAIL_EMISSION_REWARD                          = UINT64_C(10000000000);
+const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(20000000000000000);
 const size_t CRYPTONOTE_COIN_VERSION                         = 1;
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
